8aa81c416ede8767606d2e1f0d7de47b2232c8eb
Shashank Rathi <100426885+teadrinke@users.noreply.github.com>
2025-03-20 08:11:24 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,31 @@ let someNumber = 33
 
 let stringNumber = String(33)
 console.log(typeof stringNumber)
+
+//----------OPERATIONS--------------------
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " shanks"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2")
+//numbers after string are converted to string
+//where as numbers before string are added normally
+
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
